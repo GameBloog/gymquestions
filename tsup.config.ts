42727ab14f940efea5060f00fdae5a6d98c03e49
Pedro Gimenez <gimenezcavalcantepedro@gmail.com>
2025-11-26 03:55:06 -0300

    add tsup

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+  outDir: "dist",
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  clean: true,
+})
